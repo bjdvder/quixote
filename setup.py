89943645ebb9b291b8cba66af65642b1bdcb4ff5
Chi <clvrobj@gmail.com>
2013-01-09 09:46:14 +0800

    ci ci dj

--- setup.py
+++ setup.py
@@ -1,74 +1,100 @@
-#!/usr/bin/env python
-#try:
-#    from setuptools import setup
-#except ImportError:
-#    print '(WARNING: importing distutils, not setuptools!)'
-#    from distutils.core import setup
-
-# Setup script for Quixote
-
-import sys
+from distutils.core import setup
+from distutils.command.install_data import install_data
+from distutils.command.install import INSTALL_SCHEMES
 import os
-from distutils import core
-from distutils.extension import Extension
-from quixote.ptl.qx_distutils import qx_build_py
-from quixote import __version__
+import sys
 
-# a fast htmltext type
-htmltext = Extension(name="quixote.html._c_htmltext",
-                     sources=["quixote/html/_c_htmltext.c"])
+class osx_install_data(install_data):
+    # On MacOS, the platform-specific lib dir is /System/Library/Framework/Python/.../
+    # which is wrong. Python 2.5 supplied with MacOS 10.5 has an Apple-specific fix
+    # for this in distutils.command.install_data#306. It fixes install_lib but not
+    # install_data, which is why we roll our own install_data class.
 
-# faster import hook for PTL modules
-cimport = Extension(name="quixote.ptl.cimport",
-                    sources=["quixote/ptl/cimport.c"])
+    def finalize_options(self):
+        # By the time finalize_options is called, install.install_lib is set to the
+        # fixed directory, so we set the installdir to install_lib. The
+        # install_data class uses ('install_data', 'install_dir') instead.
+        self.set_undefined_options('install', ('install_lib', 'install_dir'))
+        install_data.finalize_options(self)
 
-kw = {'name': "Quixote",
-      'version': __version__,
-      'description': "A small and flexible Python Web application framework",
-      'author': "The Quixote developers",
-      'author_email': "webmaster@quixote.ca",
-      'url': "http://www.quixote.ca/",
-      'license': "DFSG approved (see LICENSE.txt)",
+if sys.platform == "darwin":
+    cmdclasses = {'install_data': osx_install_data}
+else:
+    cmdclasses = {'install_data': install_data}
 
-      'package_dir': {'quixote': 'quixote'},
-      'packages': ['quixote',  'quixote.demo', 'quixote.form',
-                   'quixote.html', 'quixote.ptl',
-                   'quixote.server'],
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
 
-      'ext_modules': [],
+# Tell distutils not to put the data_files in platform-specific installation
+# locations. See here for an explanation:
+# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
 
-      'cmdclass': {'build_py': qx_build_py},
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+django_dir = 'django'
 
-#      'test_suite' : 'nose.collector'
-     }
+for dirpath, dirnames, filenames in os.walk(django_dir):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if '__init__.py' in filenames:
+        packages.append('.'.join(fullsplit(dirpath)))
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
 
+# Small hack for working with bdist_wininst.
+# See http://mail.python.org/pipermail/distutils-sig/2004-August/004134.html
+if len(sys.argv) > 1 and sys.argv[1] == 'bdist_wininst':
+    for file_info in data_files:
+        file_info[0] = '\\PURELIB\\%s' % file_info[0]
 
-build_extensions = sys.platform != 'win32'
+# Dynamically calculate the version based on django.VERSION.
+version = __import__('django').get_version()
 
-if build_extensions:
-    # The _c_htmltext module requires Python 2.2 features.
-    if sys.hexversion >= 0x20200a1:
-        kw['ext_modules'].append(htmltext)
-    kw['ext_modules'].append(cimport)
-
-# If we're running Python 2.3, add extra information
-if hasattr(core, 'setup_keywords'):
-    if 'classifiers' in core.setup_keywords:
-        kw['classifiers'] = ['Development Status :: 5 - Production/Stable',
-          'Environment :: Web Environment',
-          'License :: DFSG approved',
-          'Intended Audience :: Developers',
-          'Operating System :: Unix',
-          'Operating System :: Microsoft :: Windows',
-          'Operating System :: MacOS :: MacOS X',
-          'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-          ]
-    if 'download_url' in core.setup_keywords:
-        kw['download_url'] = ('http://quixote.ca/releases/'
-                              'Quixote-%s.tar.gz' % kw['version'])
-    if 'url' in core.setup_keywords:
-        kw['url'] = 'http://www.quixote.ca/'
-    if 'platforms' in core.setup_keywords:
-        kw['platforms'] = 'Most'
-
-core.setup(**kw)
+setup(
+    name = "Django",
+    version = version,
+    url = 'http://www.djangoproject.com/',
+    author = 'Django Software Foundation',
+    author_email = 'foundation@djangoproject.com',
+    description = 'A high-level Python Web framework that encourages rapid development and clean, pragmatic design.',
+    download_url = 'https://www.djangoproject.com/m/releases/1.4/Django-1.4.1.tar.gz',
+    packages = packages,
+    cmdclass = cmdclasses,
+    data_files = data_files,
+    scripts = ['django/bin/django-admin.py'],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+   ],
+)
